f782c379ae6afc6e6e5bc43962919ecf83f0843a
apuitiza <anthony.puitiza.02@gmail.com>
2018-07-25 10:36:59 -0500

    De esta forma ya se crea un navigation drawer con fragments

--- app/src/main/java/com/lucky/navigationdrawerwithfragment/MainActivity.java
+++ app/src/main/java/com/lucky/navigationdrawerwithfragment/MainActivity.java
@@ -1,13 +1,21 @@
 package com.lucky.navigationdrawerwithfragment;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.lucky.navigationdrawerwithfragment.fragments.ChatFragment;
+import com.lucky.navigationdrawerwithfragment.fragments.MessageFragment;
+import com.lucky.navigationdrawerwithfragment.fragments.ProfileFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -18,14 +26,21 @@ public class MainActivity extends AppCompatActivity {
 // -------------------------Se setea el toolbar------------------------------------------------------------
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-// --------------------------------------------------------------------------------------------------
-
+// -------------------------------------------------------------------------------------------------------
+// -----------Esto es para implementar los metodos cuando se selecciona un item----------------------------
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+// -------------------------------------------------------------------------------------------------------
         drawer = findViewById(R.id.drawer_layout);
-
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MessageFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_message);
+        }
     }
 
     /**
@@ -38,5 +53,34 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+    /**
+     * Segun lo que selecciona en el navigation drawer comienza a hacer
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+            case R.id.nav_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ChatFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+// Luego de seleccionar un item tiene que cerrarse el drawer
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
